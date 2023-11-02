66c4e7e42aa4a4ce1e66b37501baf11415aeecdf
Md Yousuf Ali <yousuf3usb@gmail.com>
2023-11-02 22:57:16 +0600

    implement an api to update a movie

--- main.go
+++ main.go
@@ -52,12 +52,30 @@ func main() {
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("starting server at port 8080")
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	addHeader(w)
+	id := mux.Vars(r)["id"]
+
+	for index, movie := range movies {
+		if movie.ID == id {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = id
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
 	}
 }
 
